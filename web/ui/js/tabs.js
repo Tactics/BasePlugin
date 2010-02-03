db0ea60268d65d5617b14d1b78f22ae11c80efa0
gert <gert@f972ef24-6785-6244-9f4f-f595f474ce1c>
2010-02-03 16:49:38 +0000

    [FEATURE] combi html en ajax tabs

--- web/ui/js/tabs.js
+++ web/ui/js/tabs.js
@@ -2,16 +2,37 @@
 
   $.tt = $.tt || {};
   
+  $.tools.tabs.addEffect('tt_custom',  function(i, done) {
+		// load via ajax if href is an url
+    if (this.getTabs().eq(i).find('a').attr("href").substring(0,1) != "#")
+    {
+      this.getPanes().hide().eq(i).load(this.getTabs().eq(i).find('a').attr("href"), done).show();
+    }
+    // default: show pane
+    else
+    {
+      this.getPanes().hide().eq(i).show();
+  		done.call();
+    }
+	}); 
+  
   $.fn.tt_tabs = function(options)
   {
     $this = $(this);
-		options = $.extend({}, options);
+		options = $.extend({tabs: 'li', effect: 'tt_custom'}, options);
     
-    $this
-      .addClass('tt-tabpanes')
-      .children('ul:first')
-      .addClass('tt-tabs')
-      .tabs($this.children('div'), {tabs: 'li'}); //.history();)
+    // Not yet tabs
+    if (! $this.hasClass("tt-tabpanes"))
+    {
+      return $this
+        .addClass('tt-tabpanes')
+        .children('ul:first')
+        .addClass('tt-tabs')
+        .tabs($this.children('div'), options); //.history();)
+    }
+          
+    // Returns api (if no options)
+    return $this.tabs(options);
   }
   
 })(jQuery);
