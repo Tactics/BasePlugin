89d807c8294db002e765df9800d1b059337369e6
aaron <aaron@f972ef24-6785-6244-9f4f-f595f474ce1c>
2013-08-13 14:38:11 +0000

    Remove tons of whitespace and create a csv headers method.

--- lib/Misc.class.php
+++ lib/Misc.class.php
@@ -2,14 +2,14 @@
 
 /*
  * Dit bestand maakt deel uit van een applicatie voor Digipolis Antwerpen.
- * 
+ *
  * (c) 2008 Tactics BVBA
  *
  * Recht werd verleend om dit bestand te gebruiken als onderdeel van de genoemde
  * applicatie. Mag niet doorverkocht worden, noch rechtstreeks noch via een
- * derde partij. Meer informatie in het desbetreffende aankoopcontract. 
+ * derde partij. Meer informatie in het desbetreffende aankoopcontract.
  */
- 
+
 /**
  * Bevat een aantal functies die nergens anders thuis horen
  *
@@ -26,9 +26,9 @@ class Misc
    */
     public static function use_helper()
     {
-			sfLoader::loadHelpers(func_get_args(), sfContext::getInstance()->getModuleName());      
+			sfLoader::loadHelpers(func_get_args(), sfContext::getInstance()->getModuleName());
     }
-    
+
 
   /**
    * Misc::create_uuid()
@@ -52,13 +52,13 @@ class Misc
                 .substr($charid,12, 4).$hyphen
                 .substr($charid,16, 4).$hyphen
                 .substr($charid,20,12);
-                
+
         return $uuid;
       }
   }
-  
+
   /**
-   * Is the given UUID valid? 
+   * Is the given UUID valid?
    *
    * From: sfPropelUuidBehaviorPlugin-0.9.1
    * Copyright (c) 2007 Tristan Rivoallan
@@ -66,7 +66,7 @@ class Misc
   public static function is_uuid($uuid)
   {
     $pattern = '/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/';
-    return (bool)preg_match($pattern, $uuid);       
+    return (bool)preg_match($pattern, $uuid);
   }
 
 
@@ -74,7 +74,7 @@ class Misc
    * Genereer een random wachtwoord op basis van lengte
    *
    * @param integer $length (optional) Lengte van het gevraagde wachtwoord
-   * 
+   *
    * @return string Random wachtwoord
    */
   public static function create_password($length = 7)
@@ -91,13 +91,13 @@ class Misc
         $i++;
     }
 
-    return $pass;    
+    return $pass;
   }
-  
-  
+
+
   /**
    * genereer een array van de gegeven $objects, geindexeerd op basis van de gegeven $indexGetter
-   * 
+   *
    * @param array $objects : objects to cache
    * @param string $indexGetter : get method om de index van de cached objects op te halen, default de id van het object
    * @param string $valueGetter : get method om een value van de objects op te halen ipv heel het object
@@ -105,19 +105,19 @@ class Misc
    */
   public static function buildIndexedCache($objects, $indexGetter = 'getId()', $valueGetter = null)
   {
-    if (empty($objects)) 
+    if (empty($objects))
       return array();
-    
+
     $keys = eval("return array_map(create_function('\$object', 'return \$object->{$indexGetter};'), \$objects);");
-    
-    $values = $valueGetter 
+
+    $values = $valueGetter
       ? eval("return array_map(create_function('\$object', 'return \$object->{$valueGetter};'), \$objects);")
       : $objects
-    ;    
-    
+    ;
+
     return array_combine($keys, $values);
   }
-  
+
   /**
    * Zet browsersheaders zo dat het document als MS XLS bestand herkend wordt
    */
@@ -131,21 +131,32 @@ class Misc
     $response->addCacheControlHttpHeader('no-cache');
     $response->setHttpHeader('Content-Disposition', 'attachment; filename=' . $filename);
   }
-  
+
+  public static function setCsvHeaders($filename)
+  {
+    $response = sfContext::getInstance()->getResponse();
+
+    $response->setContentType('text/csv; charset=utf-8');
+    $response->setHttpHeader('Content-Language', 'nl');
+    $response->addVaryHttpHeader('Accept-Language');
+    $response->addCacheControlHttpHeader('no-cache');
+    $response->setHttpHeader('Content-Disposition', 'attachment; filename=' . $filename);
+  }
+
   /**
    * geeft geslacht terug op basis van rijksregisternummer
-   * 
+   *
    * @param string $rrn
    * @return char
    */
   public static function getGeslacht($rrn)
-  {    
+  {
     return intval(substr(str_replace(' ', '', $rrn), 6, 3)) % 2 == 0 ? 'V' : 'M';
   }
-  
+
   /**
    * geeft de geboortedatum terug op basis van rijksregisternummer
-   * 
+   *
    * @param string $rrn rijksregisternummer
    * @param string $format default propeldate
    * @return string geboortedatum
@@ -161,7 +172,7 @@ class Misc
     {
       $propelDate = '19' . substr($rrn, 0, 2) . '-' . substr($rrn, 2, 2) . '-' . substr($rrn, 4, 2);
     }
-    
+
     return format_date($propelDate, $format);
   }
 }
