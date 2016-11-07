c13c3721a83fdb44a48835f97ae7feddccb9c404
Tom De Roo <tom.deroo@tactics.be>
2016-11-07 13:06:17 +0100

    Toevoegen stripRrn functionaliteit aan Misc

--- lib/Misc.class.php
+++ lib/Misc.class.php
@@ -244,4 +244,16 @@ class Misc
       fwrite($fp, $eol);
     }
   }
+
+  /**
+   * @param $rrn
+   * @return mixed
+   */
+  public static function stripRrn($rrn) {
+    $rrn = str_replace(' ', '', $rrn);
+    $rrn = str_replace('.', '', $rrn);
+    $rrn = str_replace('-', '', $rrn);
+
+    return $rrn;
+  }
 }
