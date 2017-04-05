0c5371b795c02b855abb7e3c15662f4cbdad74a3
boutmansb <benjamin@tactics.be>
2017-04-05 14:31:10 +0200

    Validatie op ttAdresgegevens class toegevoegd

--- modules/ttAdres/actions/components.class.php
+++ modules/ttAdres/actions/components.class.php
@@ -3,13 +3,18 @@ class ttAdresComponents extends sfComponents
 {
   public function executeEdit()
   {
+    // Gegeven object moet van class ttAdresGegevens zijn.
+    if (get_class($this->object) != ttAdresGegevens::class)
+      throw new Exception('Het gegeven object is geen ttAdresGegevens');
+
+    // Mogelijke parameters.
     $this->prefix = isset($this->prefix) ? $this->prefix : '';
     $this->disabled = isset($this->disabled) ? $this->disabled : false;
     $this->edit_land  = isset($this->edit_land) ? $this->edit_land : true;
 
+    // Namen van de velden correct zetten met gebruik van gegeven prefix.
     $this->gemeente_id = $this->prefix .  'gemeente_id';
     $this->gemeente_zoekveld = $this->gemeente_id . '_zoekveld';
-
     $this->field_landid = $this->prefix . 'land_id';
     $this->field_postcode = $this->prefix . 'postcode';
     $this->field_gemeente = $this->prefix . 'gemeente';
