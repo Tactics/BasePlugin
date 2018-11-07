31e3cd13356054c908a3a02ef2ec113729d1f26c
Taco Orens <Taco.Orens@tactics.be>
2018-11-07 13:00:33 +0100

    [TASK] Upgrade complexitety of generating password

--- lib/Misc.class.php
+++ lib/Misc.class.php
@@ -79,19 +79,41 @@ class Misc
    */
   public static function create_password($length = 7)
   {
-    $chars = "abcdefghijkmnopqrstuvwxyz023456789";
-    srand((double)microtime()*1000000);
-    $i = 0;
-    $pass = '';
+      $add_dashes = false;
+      $available_sets = 'luds';
 
-    while ($i <= $length) {
-        $num = rand() % 33;
-        $tmp = substr($chars, $num, 1);
-        $pass = $pass . $tmp;
-        $i++;
-    }
+      $sets = array();
+      if(strpos($available_sets, 'l') !== false)
+          $sets[] = 'abcdefghjkmnpqrstuvwxyz';
+      if(strpos($available_sets, 'u') !== false)
+          $sets[] = 'ABCDEFGHJKMNPQRSTUVWXYZ';
+      if(strpos($available_sets, 'd') !== false)
+          $sets[] = '23456789';
+      if(strpos($available_sets, 's') !== false)
+          $sets[] = '!@#$%&*?';
+      $all = '';
+      $password = '';
+      foreach($sets as $set)
+      {
+          $password .= $set[array_rand(str_split($set))];
+          $all .= $set;
+      }
+      $all = str_split($all);
+      for($i = 0; $i < $length - count($sets); $i++)
+          $password .= $all[array_rand($all)];
+      $password = str_shuffle($password);
+      if(!$add_dashes)
+          return $password;
+      $dash_len = floor(sqrt($length));
+      $dash_str = '';
+      while(strlen($password) > $dash_len)
+      {
+          $dash_str .= substr($password, 0, $dash_len) . '-';
+          $password = substr($password, $dash_len);
+      }
+      $dash_str .= $password;
 
-    return $pass;
+    return $dash_str;
   }
 
 
